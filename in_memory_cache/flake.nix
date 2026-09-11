d5b189484d51d6cb098a4d3db6a1e9d6ee741d0f
faiq1818 <ghozyerlanggafaiq@gmail.com>
2026-09-12 06:44:48 +0700

    feat: add in_memory_cache projects

---
+++ in_memory_cache/flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gnumake
+            ];
+            shellHook = ''
+              exec zsh
+            '';
+          };
+        };
+    };
+}
---
+++ in_memory_cache/producer/go.mod
@@ -0,0 +1,3 @@
+module github.com/faiq1818/skripsi/in_memory_cache/producer
+
+go 1.26.3
